b6bd28648eaed8f7048f43b230375d6136b87428
Frame1910 <dpmeiring@gmail.com>
2018-12-18 22:01:23 +0800

    Created Select quantity function completed a function that selects the quantityt of product

--- shoebot.py
+++ shoebot.py
@@ -5,6 +5,9 @@ import csv
 import re
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Fake Headers: headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
@@ -23,8 +26,6 @@ def CheckStock(url):
     # These methods DO NOT work if desired elements are rendered with JavaScipt
     # hdr = {'User-Agent': 'Mozilla/5.0'}
     # req = requests.get(url, headers=hdr)\
-    driver = webdriver.Chrome('C:/Users/dpmei/Documents/GitHub/sneaker-bot/chromedriver')
-    driver.get(url)
     req = driver.page_source
     page = bs4.BeautifulSoup(req, "html.parser")
     title = page.title.string
@@ -43,9 +44,32 @@ def CheckStock(url):
     print(ArrayOfSizes)
 
 
-def Main(model, size):
+def selectQuantity(url, no):
+    dropdownSelector = "#app > div > div:nth-child(1) > div.empty_pdp_space_reserver___IFQzq > div > div.hero___2YuNz > div.container.hero_container___nM-YT > div.order_information___z33d1.col-s-12.col-l-8.col-hg-7 > div > div > form > div.row.no-gutters.size_quantity_row___1pgH7 > div.quantity_selector___1qWYG.col-s-3 > div > div"
+    selectionSelectorAddon = " > div.gl-dropdown__options > ul > li:nth-child"
+    quantityDrop = driver.find_element_by_css_selector(dropdownSelector)
+    quantityDrop.click()
+    num = str(no)
+    quantitySelect = driver.find_element_by_css_selector(dropdownSelector + selectionSelectorAddon + "(" + num + ")")
+    quantitySelect.click()
+
+
+def submit():
+    element = driver.find_element_by_css_selector('#app > div > div:nth-child(1) > div.empty_pdp_space_reserver___IFQzq > div > div.hero___2YuNz > div.container.hero_container___nM-YT > div.order_information___z33d1.col-s-12.col-l-8.col-hg-7 > div > div > form > div.row.no-gutters.add_to_bag_container___16ts0 > button')
+    element.click()
+    try:
+        element = WebDriverWait(driver, 5).until(EC.presence_of_element_located(By.CSS_SELECTOR, "#modal-root > div.gl-modal.gl-modal--regular.gl-modal--mobile-full.gl-modal--active.glass-modal___1JNyq > div.gl-modal__dialog.no-gutters.col-l-12 > div > div > div > div.row.no-gutters.gl-hidden-s-m.undefined > div.col-l-12 > div > a.gl-cta.gl-cta--primary.gl-cta--full-width.gl-vspacing-s"))
+    finally:
+        viewbag = driver.find_element_by_css_selector("#modal-root > div.gl-modal.gl-modal--regular.gl-modal--mobile-full.gl-modal--active.glass-modal___1JNyq > div.gl-modal__dialog.no-gutters.col-l-12 > div > div > div > div.row.no-gutters.gl-hidden-s-m.undefined > div.col-l-12 > div > a.gl-cta.gl-cta--primary.gl-cta--full-width.gl-vspacing-s")
+        viewbag.click()
+
+
+def Main(model, size, quantity):
     URL = urlgen(model, size)
-    CheckStock(URL)
+    driver.get(URL)
+    #CheckStock(URL)
+    selectQuantity(URL, quantity)
+    submit()
 
-
-Main('AQ0943', 8)
+driver = webdriver.Chrome('C:/Users/dpmei/Documents/GitHub/sneaker-bot/chromedriver')
+Main('AQ0943', 8, 2)
